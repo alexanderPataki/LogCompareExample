c3ae09697f7a38b8a3b7305c3094294a9f12092b
Mateus Queiroz Correia <mateus_queiroz@hotmail.com>
2016-01-30 15:00:34 -0200

    Exemplos de log para o log4net e NLog

---
+++ Log4NetExample/LogHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetExample
+{
+    public class LogHelper
+    {
+        /// <summary>
+        /// http://www.thomaslevesque.com/2012/06/13/using-c-5-caller-info-attributes-when-targeting-earlier-versions-of-the-net-framework/
+        /// https://msdn.microsoft.com/pt-br/library/system.runtime.compilerservices.callerfilepathattribute%28v=vs.110%29.aspx
+        /// </summary>
+        /// <param name="fileName">Valor definido em momento de compilacao</param>
+        /// <returns></returns>
+        public static log4net.ILog GetLogger([CallerFilePath] string fileName = "")
+        {
+            return log4net.LogManager.GetLogger(fileName);
+        }
+    }
+}
---
+++ Log4NetExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace Log4NetExample
+{
+    class Program
+    {
+        //Utilizar o reflection eh mais lento, mas isso eh feito uma vez por classe (Para framework anteriores a 4.5)
+        //private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        //Para Framework 4.5 ou superior (Definindo o log em momento de compilacao)
+        private static readonly log4net.ILog log = LogHelper.GetLogger();
+
+        static void Main(string[] args)
+        {
+            log.Debug("Mensagem de debug");
+            log.Info("Mensagem de info");
+            log.Warn("Mensagem de warning");
+
+            int i = 0;
+            try
+            {
+                int result = 10 / i;
+            }
+            catch(DivideByZeroException e)
+            {
+                log.Error("Mensagem de Erro", e);
+            }
+
+            log.Fatal("Mensagem de erro Fatal");
+
+            Console.ReadLine();
+        }
+    }
+}
